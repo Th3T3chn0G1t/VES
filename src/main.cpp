02ab132a314b887e183e553fcb0ee8e510aa30f8
VinaCC <21315274+EngineLessCC@users.noreply.github.com>
2022-05-26 21:18:14 +0200

    vcpkg integration

---
+++ src/main.cpp
@@ -0,0 +1,52 @@
+#include <fmt/format.h>
+#include <raylib.h>
+#include <raymath.h>
+#include <cmath>
+
+// Note to self: do not mix anything raylib with winapi (-> no global asio headers either :(.)
+
+int main() {
+    float mouse_sense = 5.0f;
+    float move_speed = 1.0f;
+
+    Vector2 screen_dim = {640, 480};
+    InitWindow(screen_dim.x, screen_dim.y, "VES");
+    SetTargetFPS(60);
+
+    Vector3 camera_rotation = {0.0f, 0.0f, 0.0f};
+    Vector3 camera_forward = {0.0f, 0.0f, 0.0f};
+    Camera3D camera = {{0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}, {0.0f, 1.0f, 0.0f}, 45.0f, CAMERA_PERSPECTIVE};
+
+    SetCameraMode(camera, CAMERA_CUSTOM);
+
+    DisableCursor();
+
+    while (!WindowShouldClose()) {
+        camera_forward = (Vector3){static_cast<float>(std::sin(camera_rotation.x)), static_cast<float>(std::tan(camera_rotation.y)), static_cast<float>(std::sin(M_PI_2 - camera_rotation.x))};
+        camera.target = (Vector3){camera.position.x + camera_forward.x, camera.position.y + camera_forward.y, camera.position.z + camera_forward.z};
+
+        Vector2 mouse_delta = GetMouseDelta();
+        camera_rotation.x += ((-mouse_delta.x / screen_dim.x) / (2 * M_PI)) * mouse_sense;
+        camera_rotation.y += ((-mouse_delta.y / screen_dim.y) / (2 * M_PI)) * mouse_sense;
+
+        if (IsKeyDown(KEY_W))
+            camera.position = (Vector3){camera.position.x + (camera_forward.x * move_speed), camera.position.y + (camera_forward.y * move_speed), camera.position.z + (camera_forward.z * move_speed)};
+        if (IsKeyDown(KEY_S))
+            camera.position = (Vector3){camera.position.x - (camera_forward.x * move_speed), camera.position.y - (camera_forward.y * move_speed), camera.position.z - (camera_forward.z * move_speed)};
+
+        UpdateCamera(&camera);
+
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        BeginMode3D(camera);
+        DrawCube((Vector3){10.0f, 0.0f, 0.0f}, 2.0f, 2.0f, 2.0f, RED);
+        DrawCube((Vector3){0.0f, 0.0f, -10.0f}, 2.0f, 2.0f, 2.0f, BLUE);
+        DrawCube((Vector3){0.0f, 0.0f, 10.0f}, 2.0f, 2.0f, 2.0f, GREEN);
+        DrawCube((Vector3){-10.0f, 0.0f, 0.0f}, 2.0f, 2.0f, 2.0f, YELLOW);
+        EndMode3D();
+        EndDrawing();
+    }
+
+    CloseWindow();
+}
---
+++ vcpkg.cmake
@@ -0,0 +1,26 @@
+cmake_minimum_required(VERSION 3.18.1)
+
+set(VCPKG_INSTALLED_DIR "${CMAKE_CURRENT_SOURCE_DIR}/build/vcpkg")
+
+if (NOT DEFINED VCPKG_TARGET_TRIPLET)
+    if (WIN32)
+        set(VCPKG_TARGET_TRIPLET "x64-windows-static")
+    elseif(UNIX)
+        set(VCPKG_TARGET_TRIPLET "x64-linux")
+    else()
+        message(FATAL_ERROR "Unsupported platform")
+    endif()
+endif()
+
+if (DEFINED VCPKG_ROOT)
+    set(CMAKE_TOOLCHAIN_FILE "${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" CACHE STRING "" FORCE)
+elseif (DEFINED ENV{VCPKG_ROOT})
+    set(CMAKE_TOOLCHAIN_FILE "$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" CACHE STRING "" FORCE)
+else()
+    message(FATAL_ERROR "Please set VCPKG_ROOT(environment or cmake) to your vcpkg installation")
+endif()
+
+set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
+if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+    add_definitions(/MP)
+endif()
